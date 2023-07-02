4d698d66019ab5f8d53cc41f7ae3eadabba7e658
YamanakaRui <k021g1123@g.neec.ac.jp>
2023-07-02 23:41:37 +0900

    プロト前最終プッシュ

--- Application/Draw/Actor/HitActor.cpp
+++ Application/Draw/Actor/HitActor.cpp
@@ -1,1 +1,26 @@
 #include "HitActor.h"
+#include <cassert>
+
+using YGame::HitActor;
+using YMath::Vector3;
+using YMath::Vector4;
+
+void HitActor::Initialize(const Vector4& hitColor)
+{
+	shake_.Initialize();
+
+	hitColor_ = hitColor;
+}
+
+void HitActor::Update()
+{
+	if (isAct_ == false) { return; }
+
+	shake_.Update();
+}
+
+void HitActor::Hit(const float swing, const float dekey, const float place)
+{
+	shake_.Activate(swing, dekey, place);
+}
+
--- Application/Draw/Actor/HitActor.h
+++ Application/Draw/Actor/HitActor.h
@@ -1,5 +1,40 @@
 #pragma once
-class HitActor
-{
-};
+#include "Shake.h"
+#include "Vector4.h"
+#include <cstdint>
 
+namespace YGame
+{
+	class HitActor
+	{
+	
+	public:
+
+		void Initialize(const YMath::Vector4& hitColor = YMath::Vector4(1.0f, 0.0f, 0.0f, 0.75f));
+
+		void Update();
+
+		void Hit(const float swing, const float dekey, const float place);
+
+		inline YMath::Vector4 HitColor() const { return hitColor_; }
+
+		inline bool IsAct() const { return isAct_; }
+
+	public:
+
+		HitActor() = default;
+		
+		virtual ~HitActor() = default;
+	
+	private:
+
+		// 動いているかフラグ
+		bool isAct_ = false;
+		
+		// 揺れ
+		YMath::Shake shake_;
+		
+		// ヒット時の色
+		YMath::Vector4 hitColor_;
+	};
+}
